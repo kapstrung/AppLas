0b31a04bf386b3f93e742859f04c50f8f016f8e4
kapstrung <andrzejbartoszek686@gmail.com>
2014-06-20 11:31:47 +0200

    Konfiguracja polaczenia z baza plik parameters.yml Fixes #3

--- src/AppLas/AppLasSiteBundle/Controller/DefaultController.php
+++ src/AppLas/AppLasSiteBundle/Controller/DefaultController.php
@@ -12,11 +12,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/mapa")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        return array('name' => "ss");
     }
 }
